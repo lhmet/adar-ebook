0e29667a80d19696e64a34b75a5c8c70f8ded9bd
lhmet <lhmet@ufsm.br>
2020-05-02 13:41:41 -0300

    code ident

--- R/shiny-detect-covid.R
+++ R/shiny-detect-covid.R
@@ -4,38 +4,38 @@ detecta_suspeito_covid <- function(info_paciente, limiar_covid = 4) {
 
   # Sindrome respiratoria aguda
   ptos1 <- ifelse(test = info_paciente$srag,
-                  yes = 4,
-                  no = 0
+    yes = 4,
+    no = 0
   )
 
   # febre
   ptos2 <- ifelse(test = info_paciente$febre > 37.8,
-                  yes = 2,
-                  no = 0
+    yes = 2,
+    no = 0
   )
 
   # tosse
   ptos3 <- ifelse(test = info_paciente$tosse == "com catarro",
-                  yes = 2,
-                  no = 0
+    yes = 2,
+    no = 0
   )
 
   # roquidao
   ptos4 <- ifelse(test = info_paciente$roquidao,
-                  yes = 1,
-                  no = 0
+    yes = 1,
+    no = 0
   )
 
   # dor de garganta
   ptos5 <- ifelse(test = info_paciente$dor_garganta,
-                  yes = 1,
-                  no = 0
+    yes = 1,
+    no = 0
   )
 
   # nariz entupido
   ptos6 <- ifelse(test = info_paciente$nariz_ent,
-                  yes = 1,
-                  no = 0
+    yes = 1,
+    no = 0
   )
 
   ptos_tot <- ptos1 + ptos2 + ptos3 + ptos4 + ptos5 + ptos6
@@ -58,12 +58,3 @@ dados_paciente <- data.frame(
 
 # aplicando a função
 detecta_suspeito_covid(info_paciente = dados_paciente)
-
-
-
-
-
-
-
-
-
